3e17311185f3c35b4995ac6a3b58300503f2baa8
Justine Ariate <157104305+justineariate@users.noreply.github.com>
2026-06-08 01:10:04 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get message box element
+const messageBox = document.getElementById('messageBox');
+
+// Toggle message open/close
+function toggleMessage() {
+    messageBox.classList.toggle('opened');
+}
